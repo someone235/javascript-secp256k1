eba88274376cd211c37989465370c940b4f57907
Elichai Turkel <elichai.turkel@gmail.com>
2021-02-15 12:59:50 +0200

    Move code into a lib directory

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 ./depend/configure.sh
-emcc -O3 -s WASM=1 -I./depend/secp256k1/src -I./depend/secp256k1 -I./depend/secp256k1/include -DHAVE_CONFIG_H -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' ./depend/secp256k1/src/secp256k1.c wrapper.c -o libsecp256k1.js
+emcc -O3 -s WASM=1 -I./depend/secp256k1/src -I./depend/secp256k1 -I./depend/secp256k1/include -DHAVE_CONFIG_H -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' ./depend/secp256k1/src/secp256k1.c ./lib/wrapper.c -o ./lib/libsecp256k1.js
