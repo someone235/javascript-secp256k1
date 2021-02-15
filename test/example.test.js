5d95b7397626919454f73e8872e46a4124a96c57
Elichai Turkel <elichai.turkel@gmail.com>
2021-02-15 20:20:45 +0200

    Add tests for the new wrappers

--- test/example.test.js
+++ test/example.test.js
@@ -1,23 +1,26 @@
 const tap = require('tap')
 const Module = require("../lib/libsecp256k1.node.js")
+const crypto = require('crypto');
 
 tap.test(async t => {
+    // Should only be loaded once
     const loadedModule = await Module();
 
     const api = {
         context_create: loadedModule.cwrap('context_create', 'number', []),
         context_destroy: loadedModule.cwrap('context_destroy', '', ['number']),
         seckey_to_pubkey: loadedModule.cwrap('seckey_to_pubkey', 'number', ['number', 'number', 'array']),
-        schnorrsig_sign: loadedModule.cwrap('schnorrsig_sign', 'number', ['number', 'array', 'array', 'array', 'array']),
+        schnorrsig_sign: loadedModule.cwrap('schnorrsig_sign', 'number', ['number', 'number', 'array', 'array', 'array']),
         schnorrsig_verify: loadedModule.cwrap('schnorrsig_verify', 'number', ['number', 'array', 'array', 'array']),
     };
 
     // global context variable, initialized once and used for the full duration of the program.
+    // should only be created once
     var context = api.context_create();
 
     function seckey_to_pubkey(seckey) {
         if (seckey.length != 32) {
-            throw `invalid secret key length(${seckey}), expected 32 bytes`
+            throw `invalid secret key length(${seckey.length}), expected 32 bytes`
         }
         const wasmMemoryPublicKey = loadedModule._malloc(32);
         const res = api.seckey_to_pubkey(context, wasmMemoryPublicKey, seckey);
@@ -30,8 +33,60 @@ tap.test(async t => {
         return pubkey
     }
 
-    var onesSecretKey = new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    var pubkey = seckey_to_pubkey(onesSecretKey);
-    var pubkeyHex = Buffer.from(pubkey).toString('hex');
-    t.equal(pubkeyHex, "1b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f")
+    function schnorrsig_sign(secretKey, messageHash) {
+        if (seckey.length != 32) {
+            throw `invalid secret key length(${seckey.length}), expected 32 bytes`
+        }
+        if (messageHash.length != 32) {
+            throw `invalid messageHash length(${messageHash.length}), expected 32 bytes`
+        }
+        const rand32 = crypto.randomBytes(32);
+        const wasmMemorySignature = loadedModule._malloc(64);
+
+        const res = api.schnorrsig_sign(context, wasmMemorySignature, messageHash, secretKey, rand32);
+        if (res != 1) {
+            throw 'invalid secret key'
+        }
+        var signature = new Uint8Array(64)
+        signature.set(new Uint8Array(loadedModule.HEAPU8.buffer, wasmMemorySignature, 64))
+        loadedModule._free(wasmMemorySignature)
+        return signature
+    }
+
+    function schnorrsig_verify(publicKey, messageHash, signature) {
+        if (publicKey.length != 32) {
+            throw `invalid public key length(${publicKey.length}), expected 32 bytes`
+        }
+        if (messageHash.length != 32) {
+            throw `invalid messageHash length(${messageHash.length}), expected 32 bytes`
+        }
+        if (signature.length != 64) {
+            throw `invalid signature length(${messageHash.length}), expected 64 bytes`
+        }
+        const res = api.schnorrsig_verify(context, signature, messageHash, publicKey);
+        return res == 1
+    }
+
+    var seckey = Buffer.from('B7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF', 'hex')
+    var pubkey = seckey_to_pubkey(seckey);
+    var pubkeyHex = Buffer.from(pubkey).toString('hex').toUpperCase();
+    t.equal(pubkeyHex, 'DFF1D77F2A671C5F36183726DB2341BE58FEAE1DA2DECED843240F7B502BA659')
+
+    var messageHash = Buffer.from('243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89', 'hex')
+    var expectedSig = '6896BD60EEAE296DB48A229FF71DFE071BDE413E6D43F917DC8DCF8C78DE33418906D11AC976ABCCB20B091292BFF4EA897EFCB639EA871CFA95F6DE339E4B0A'
+    var auxRand = Buffer.from('0000000000000000000000000000000000000000000000000000000000000001', 'hex')
+    const wasmMemorySignature = loadedModule._malloc(64);
+    const res = api.schnorrsig_sign(context, wasmMemorySignature, messageHash, seckey, auxRand);
+    if (res != 1) {
+        throw 'invalid secret key'
+    }
+    var sigHex = Buffer.from((new Uint8Array(loadedModule.HEAPU8.buffer, wasmMemorySignature, 64))).toString('hex').toUpperCase()
+    t.equal(expectedSig, sigHex)
+    loadedModule._free(wasmMemorySignature)
+
+    var sig = schnorrsig_sign(seckey, messageHash)
+    t.ok(schnorrsig_verify(pubkey, messageHash, sig))
+
+    sig[30] += 1
+    t.notOk(schnorrsig_verify(pubkey, messageHash, sig))
 })
