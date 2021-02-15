a8a4bf4eaa920058de98d1977221c93b798f0e02
Elichai Turkel <elichai.turkel@gmail.com>
2021-02-15 14:30:46 +0200

    Update the compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
-./depend/configure.sh
-emcc -O3 -s WASM=1 -I./depend/secp256k1/src -I./depend/secp256k1 -I./depend/secp256k1/include -DHAVE_CONFIG_H -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' ./depend/secp256k1/src/secp256k1.c ./lib/wrapper.c -o ./lib/libsecp256k1.js
+pushd depend
+./configure.sh
+emcc -O3 -s WASM=1 -I./secp256k1/src -I./secp256k1 -I./secp256k1/include -DHAVE_CONFIG_H -s -s EXPORTED_FUNCTIONS='["_malloc", "_free"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' ./secp256k1/src/secp256k1.c ../lib/wrapper.c -o ../lib/libsecp256k1.js
